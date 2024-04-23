5e712f51d306638d18cf7bd5e6e4db164c412398
nithin <91445424+nannithin@users.noreply.github.com>
2024-04-23 10:31:05 +0530

    Update signup.jsx

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
 
     const submithandler = (e) => {
         e.preventDefault();
-        axios.post('https://sem-proj-blog.vercel.app/api/auth/signup',{name,email,password},{withCredentials : true})
+        axios.post('https://sem-proj-blog.onrender.com/api/auth/signup',{name,email,password},{withCredentials : true})
         .then(({ data }) => {
             if (data.status == 'sucess') {
                 localStorage.setItem('user', JSON.stringify(data))
