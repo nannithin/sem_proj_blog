82452253867cbb42a839cac0b7a9304fb6c02970
nithin <91445424+nannithin@users.noreply.github.com>
2024-04-23 10:26:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:["https://blogger-roan.vercel.app"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials: true
+}))
 app.use(cookieParser());
 
 
